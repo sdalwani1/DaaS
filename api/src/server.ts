edc5ed12875c76398a904facba4a6f4f9d018773
unknown <35954019+sdalwani1@users.noreply.github.com>
2026-09-25 15:42:12 +0530

    (api): apollo server hello world

---
+++ api/src/server.ts
@@ -0,0 +1,22 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello from the DaaS API",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: Number(process.env.PORT) || 4000 },
+});
+
+console.log(`API ready at ${url}`);
